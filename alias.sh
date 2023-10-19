3c4496784ec0fa2d201a8b518151bd36d34b0b7b
= <=>
2023-10-19 16:40:59 +0100

    add basic directory listing aliases

---
+++ alias.sh
@@ -0,0 +1,6 @@
+# Directory listing aliases
+echo "alias ll='ls -lF'" >> ~/.bashrc
+echo "alias la='ls -alF'" >> ~/.bashrc
+echo "alias lt='ls -ltF'" >> ~/.bashrc
+echo "alias lat='ls -latF'" >> ~/.bashrc
+
